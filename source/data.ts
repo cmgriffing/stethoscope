76353213139e544d0f4577dfc18589d5356d2b81
Chris Griffing <cmgriffing@gmail.com>
2021-11-07 13:49:22 -0800

    feat: allow users to enable and disable criteria

--- source/data.ts
+++ source/data.ts
@@ -1,101 +1,178 @@
-import { GradeDetails, ScoreDetails } from "./types";
+import {
+  GradeDetails,
+  ScoreConfig,
+  ScoreConfigValueType,
+  ScoreDetails,
+  ScoreKey,
+} from "./types";
 
 export const gradeDetails: GradeDetails = {
-	F: {
-		minimumScore: 0,
-		color: "red",
-	},
-	D: {
-		minimumScore: 60,
-		color: "orange",
-	},
-	C: {
-		minimumScore: 70,
-		color: "yellow",
-	},
-	B: {
-		minimumScore: 80,
-		color: "lightgreen",
-	},
-	A: {
-		minimumScore: 90,
-		color: "green",
-	},
+  F: {
+    minimumScore: 0,
+    color: "red",
+  },
+  D: {
+    minimumScore: 60,
+    color: "orange",
+  },
+  C: {
+    minimumScore: 70,
+    color: "yellow",
+  },
+  B: {
+    minimumScore: 80,
+    color: "lightgreen",
+  },
+  A: {
+    minimumScore: 90,
+    color: "green",
+  },
+};
+
+export const defaultScoreConfig: ScoreConfig = {
+  modifiedDate: 0,
+  values: {
+    [ScoreKey.stars]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 5,
+      enabled: true,
+    },
+
+    [ScoreKey.readme]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 30,
+      enabled: true,
+    },
+
+    [ScoreKey.mostRecentCommit]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 20,
+      enabled: true,
+    },
+    [ScoreKey.amountOfContributors]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 10,
+      enabled: true,
+    },
+    [ScoreKey.amountOfCommits]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 15,
+      enabled: true,
+    },
+
+    [ScoreKey.sponsors]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 5,
+      enabled: true,
+    },
+
+    [ScoreKey.releases]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 5,
+      enabled: true,
+    },
+
+    [ScoreKey.docsFolder]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 5,
+      enabled: true,
+    },
+
+    [ScoreKey.buildPassing]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 10,
+      enabled: true,
+    },
+
+    [ScoreKey.commitFrequency]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 10,
+      enabled: true,
+    },
+
+    [ScoreKey.openIssues]: {
+      valueType: ScoreConfigValueType.Number,
+      value: 10,
+      enabled: true,
+    },
+  },
 };
 
 export function createScoreDetails(): ScoreDetails {
-	return {
-		repoName: "",
-		authorName: "",
-		hasLicense: false,
-		hasAmbiguousLicense: false,
-		scores: {
-			stars: {
-				value: 0,
-				score: 0,
-				maxScore: 5,
-				label: "Amount of Stars",
-			},
-			readme: {
-				value: 0,
-				score: 0,
-				maxScore: 30,
-				label: "README file exists",
-			},
-			mostRecentCommit: {
-				value: 0,
-				score: 0,
-				maxScore: 20,
-				label: "Most recent commit",
-			},
-			amountOfContributors: {
-				value: 0,
-				score: 0,
-				maxScore: 10,
-				label: "Amount of Contributors",
-			},
-			amountOfCommits: {
-				value: 0,
-				score: 0,
-				maxScore: 15,
-				label: "Amount of commits",
-			},
-			sponsors: {
-				value: 0,
-				score: 0,
-				maxScore: 5,
-				label: "Sponsor section exists",
-			},
-			releases: {
-				value: 0,
-				score: 0,
-				maxScore: 5,
-				label: "Releases section exists",
-			},
-			docsFolder: {
-				value: 0,
-				score: 0,
-				maxScore: 5,
-				label: "Docs folder exists",
-			},
-			buildPassing: {
-				value: 0,
-				score: 0,
-				maxScore: 10,
-				label: "Build passing on recent commit",
-			},
-			commitFrequency: {
-				value: 0,
-				score: 0,
-				maxScore: 10,
-				label: "Frequency of commits",
-			},
-			openIssues: {
-				value: 0,
-				score: 0,
-				maxScore: 10,
-				label: "Open issues score",
-			},
-		},
-	};
+  const { values } = defaultScoreConfig;
+
+  return {
+    repoName: "",
+    authorName: "",
+    hasLicense: false,
+    hasAmbiguousLicense: false,
+    scores: {
+      stars: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.stars].value,
+        label: "Amount of Stars",
+      },
+      readme: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.readme].value,
+        label: "README file exists",
+      },
+      mostRecentCommit: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.mostRecentCommit].value,
+        label: "Most recent commit",
+      },
+      amountOfContributors: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.amountOfContributors].value,
+        label: "Amount of Contributors",
+      },
+      amountOfCommits: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.amountOfCommits].value,
+        label: "Amount of commits",
+      },
+      sponsors: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.sponsors].value,
+        label: "Sponsor section exists",
+      },
+      releases: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.releases].value,
+        label: "Releases section exists",
+      },
+      docsFolder: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.docsFolder].value,
+        label: "Docs folder exists",
+      },
+      buildPassing: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.buildPassing].value,
+        label: "Build passing on recent commit",
+      },
+      commitFrequency: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.commitFrequency].value,
+        label: "Frequency of commits",
+      },
+      openIssues: {
+        value: 0,
+        score: 0,
+        maxScore: values[ScoreKey.openIssues].value,
+        label: "Open issues score",
+      },
+    },
+  };
 }
--- source/utils.ts
+++ source/utils.ts
@@ -1,30 +1,42 @@
 import { gradeDetails } from "./data";
-import { ScoreDetails } from "./types";
+import { ScoreConfig, ScoreDetails, ScoreKey } from "./types";
 
 export function getGradeFromScoreTotal(scoreTotal: number) {
-	let grade = "F";
-	let gradeColor = "red";
-	Object.entries(gradeDetails).forEach(([gradeKey, gradeDetail]) => {
-		if (scoreTotal >= gradeDetail.minimumScore) {
-			grade = gradeKey;
-			gradeColor = gradeDetail.color;
-		}
-	});
+  let grade = "F";
+  let gradeColor = "red";
+  Object.entries(gradeDetails).forEach(([gradeKey, gradeDetail]) => {
+    if (scoreTotal >= gradeDetail.minimumScore) {
+      grade = gradeKey;
+      gradeColor = gradeDetail.color;
+    }
+  });
 
-	return {
-		color: gradeColor,
-		letter: grade,
-	};
+  return {
+    color: gradeColor,
+    letter: grade,
+  };
 }
 
-export function getTotalFromScore(score: ScoreDetails) {
-	let scoreTotal = 0;
+export function getTotalFromScore(
+  score: ScoreDetails,
+  scoreConfig: ScoreConfig,
+  scoreTotal = 0
+) {
+  let maxPossibleScore = 0;
+  if (scoreTotal === 0 && score.hasLicense) {
+    Object.entries(score.scores).forEach(([scoreKey, scoreDetails]) => {
+      const scoreConfigDetails = scoreConfig.values[scoreKey as ScoreKey];
+      if (scoreConfigDetails.enabled) {
+        scoreTotal += scoreDetails.score;
+        maxPossibleScore += scoreConfigDetails.value;
+      }
+    });
+  }
 
-	if (score.hasLicense) {
-		Object.entries(score.scores).forEach(([_scoreKey, scoreDetails]) => {
-			scoreTotal += scoreDetails.score;
-		});
-	}
+  let adjustedScore = 100;
+  if (maxPossibleScore > 0) {
+    adjustedScore = (scoreTotal / maxPossibleScore) * 100;
+  }
 
-	return scoreTotal;
+  return adjustedScore;
 }
