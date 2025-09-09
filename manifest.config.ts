5e54ed010fe742c02872042e8c156e05b22f651b
Chris Griffing <cmgriffing@gmail.com>
2025-09-09 15:54:11 -0700

    fix: refine permissions again

--- manifest.config.ts
+++ manifest.config.ts
@@ -16,7 +16,7 @@ export default defineManifest({
     },
     default_popup: "src/popup/index.html",
   },
-  permissions: ["activeTab"],
+  permissions: ["tabs"],
   content_scripts: [
     {
       js: ["src/content/main.ts"],
